53e577801ab6a1f0fe11297ae27f3a214b5e7b0d
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2020-05-28 21:30:56 +0200

    re-add erase (write.seq) and add move (write.read)

--- src/cmdwrite.cpp
+++ src/cmdwrite.cpp
@@ -25,17 +25,52 @@ static void cmdwrite_stream( int argc, char * argv[] ) {
   if( argc==0 ) { // no arguments toggles streaming mode
     if( cmd_get_streamfunc()==0 ) cmd_set_streamfunc(cmdwrite_stream); else cmd_set_streamfunc(0);
   }
-  for( int i=0; i<argc; i++ ) {
-    uint16_t data;
-    if( !cmd_parse(argv[i],&data) || data>0xFF ) { 
-      Serial.print(F("ERROR: write: <data> must be 00..FF, not '")); 
-      Serial.print(argv[i]); Serial.print(F(", "));
-      if( i<argc-1 ) Serial.print(F("rest ")); 
-      Serial.println(F("ignored"));
-      break; // Not return, we need to update streaming prompt
+  int i;
+  for( i=0; i<argc; i++ ) {
+    if( cmd_isprefix(PSTR("seq"),argv[i]) ) { // seq <data> <num>
+      if( !(i+1<argc) ) { Serial.print(F("ERROR: write: seq: must have <data>")); goto exit;}
+      if( !(i+2<argc) ) { Serial.print(F("ERROR: write: seq: must have <data> and <num>")); i+=1; goto exit;}
+      uint16_t data, num;
+      if( !cmd_parse(argv[i+1],&data) || data>0xFF ) { Serial.print(F("ERROR: write: seq: <data> must be 00..FF")); i+=2; goto exit; }
+      if( !cmd_parse(argv[i+2],&num) ) { Serial.print(F("ERROR: write: seq: <num> must be 0000..FFFF")); i+=2; goto exit; }
+      while( num>0 ) {
+        mem_write(cmdwrite_addr, data );
+        cmdwrite_addr++; num--;
+      }
+      i+=2;
+    } else if( cmd_isprefix(PSTR("read"),argv[i]) ) { // read <addr> <num>
+      if( !(i+1<argc) ) { Serial.print(F("ERROR: write: read: must have <addr>")); goto exit;}
+      if( !(i+2<argc) ) { Serial.print(F("ERROR: write: read: must have <addr> and <num>")); i+=1; goto exit;}
+      uint16_t addr, num;
+      if( !cmd_parse(argv[i+1],&addr) ) { Serial.print(F("ERROR: write: read: <addr> must be 0000..FFFF")); i+=2; goto exit; }
+      if( !cmd_parse(argv[i+2],&num) ) { Serial.print(F("ERROR: write: read: <num> must be 0000..FFFF")); i+=2; goto exit; }
+      if( cmdwrite_addr<addr ) { // copy forward in order to not overwrite self
+        while( num>0 ) {
+          mem_write(cmdwrite_addr, mem_read(addr) );
+          cmdwrite_addr++; addr++; num--;
+        }        
+      } else { // copy backward in order to not overwrite self
+        uint16_t dest=cmdwrite_addr+num; addr+=num;
+        while( num>0 ) {
+          mem_write(dest, mem_read(addr) );
+          cmdwrite_addr++; dest--; addr--; num--;
+        }        
+      }
+      i+=2;
+    } else { // <data>
+      uint16_t data;
+      if( !cmd_parse(argv[i],&data) || data>0xFF ) { Serial.print(F("ERROR: write: <data> must be 00..FF")); goto exit; }
+      mem_write(cmdwrite_addr, data);
+      cmdwrite_addr++;
     }
-    mem_write(cmdwrite_addr, data);
-    cmdwrite_addr++;
+  }
+
+exit: // print ignore message and update prompt
+  if( i<argc ) {
+    // aborted half way with "goto exit"
+    Serial.print(F(", ")); 
+    if( i<argc-1 ) Serial.print(F("rest "));  
+    Serial.println(F("ignored"));
   }
   // Set the streaming prompt (will only be shown in streaming mode)
   char buf[8]; snprintf_P(buf,sizeof buf, PSTR("W:%04x> "),cmdwrite_addr); cmd_set_streamprompt(buf);
@@ -59,9 +94,12 @@ static const char cmdwrite_longhelp[] PROGMEM =
   "- writes <data> byte to memory location <addr>\n"
   "- multiple <data> bytes allowed (auto increment of <addr>)\n"
   "- if <data> is absent, starts streaming mode (empty line ends it)\n"
+  "- <data> can also be a 'seq' or 'read' macro\n"
+  "- use 'seq <data> <num>' to write <num> times <data>\n"
+  "- use 'read <addr> <num>' to copy <num> bytes from <addr>\n"
   "NOTE:\n"
   "- <data> is 00..FF\n"
-  "- <addr> is 0000..FFFF, but physical memory is limited and mirrored\n"
+  "- <addr> and <num> is 0000..FFFF, but physical memory is limited and mirrored\n"
 ;
 
 
