7ebd6f13a27b5d059a8574f7ae1f28662c7ae77f
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2020-06-27 18:40:40 +0200

    - more verbose errors - extended help

--- src/cmdasm.cpp
+++ src/cmdasm.cpp
@@ -12,7 +12,7 @@ static uint16_t cmdasm_addr= 0x0200; // page 0 is zero-page, page 1 is stack
 static uint16_t cmdasm_addr_first;   // for the undo feature
 
 
-// This notification called for an asm command; first changed address is passed.
+// This notification is called for an asm command; lowest changed address is passed.
 // For laziness, the handling is implemented here, not on app level
 static void cmdasm_notify(uint16_t addr) {
   // set read/dasm pointer (so that they can show what has just been written)
@@ -40,28 +40,28 @@ static void cmdasm_stream( int argc, char * argv[] ) {
     }
     cmdasm_addr= addr;
   } else {
-    // Get Mnenemonic 
+    // Get Mnemonic  
     int iix= isa_instruction_find(argv[0]);
-    if( iix==0 ) { Serial.println(F("ERROR: unknown mnenemonic")); return; }
+    if( iix==0 ) { cmd_printf_P(PSTR("ERROR: unknown mnemonic '%s'\n"),argv[0]); return; }
     // Parse operand syntax to find addressing mode
     if( argc==1 ) buf[0]='\0'; else strncpy(buf, argv[1], sizeof buf );
     int aix= isa_parse(buf);
-    if( aix==0 ) { Serial.println(F("ERROR: syntax error in operand")); return; }
+    if( aix==0 ) { cmd_printf_P(PSTR("ERROR: syntax error in operand '%s'"),buf); return; }
     // We now have instruction type and addressing mode, does the combo map to an opcode?
     bool rel_as_abs= isa_instruction_opcodes(iix,ISA_AIX_REL)!=ISA_OPCODE_INVALID && aix==ISA_AIX_ABS;
     if( rel_as_abs ) aix=ISA_AIX_REL; // We accept ABS notation for REL-only instructions
     uint8_t opcode= isa_instruction_opcodes(iix,aix);
-    if( opcode==ISA_OPCODE_INVALID ) { Serial.println(F("ERROR: instruction does not have that addressing mode")); return; }
+    if( opcode==ISA_OPCODE_INVALID ) { cmd_printf_P(PSTR("ERROR: instruction '%s' does not have addressing mode %S\n"),argv[0],isa_addrmode_aname(aix)); return; }
     // Check operand size
     uint16_t op;
     int bytes= isa_addrmode_bytes(aix);
     if( bytes>1 ) { // todo: what is buf here?
-      if( !cmd_parse(buf,&op) ) { Serial.println(F("ERROR: operand must be <hex>")); return; }
-      if( rel_as_abs ) { op= op-(cmdasm_addr+2); if( 0x7f<op && op<0xff80 ) { Serial.println(F("ERROR: ABS address too far")); return; } op&=0xFF; }
-      if( !rel_as_abs && bytes==2 && op>0xff ) { Serial.println(F("ERROR: operand must be 00..ff")); return; }
+      if( !cmd_parse(buf,&op) ) { cmd_printf_P(PSTR("ERROR: operand must be <hex>, not '%s'\n"),buf); return; }
+      if( rel_as_abs ) { op= op-(cmdasm_addr+2); if( 0x7f<op && op<0xff80 ) { cmd_printf_P(PSTR("ERROR: ABS address too far (%X), need 80..7F\n"),op); return; } op&=0xFF; }
+      if( !rel_as_abs && bytes==2 && op>0xff ) { cmd_printf_P(PSTR("ERROR: operand must be 00..ff, not '%s'\n"),buf); return; }
     }
     // Check
-    if( argc>2 ) { Serial.println(F("ERROR: text after operand")); return; }
+    if( argc>2 ) { cmd_printf_P(PSTR("ERROR: text after operand ('%s')\n"),argv[2]); return; }
     // Now assemble
     if( bytes>=1 ) { mem_write(cmdasm_addr++, opcode); }
     if( bytes>=2 ) { mem_write(cmdasm_addr++, op&0xFF); }
@@ -71,7 +71,7 @@ static void cmdasm_stream( int argc, char * argv[] ) {
     if( aix==ISA_AIX_ABS && op<0x100 ) Serial.println(F("INFO: suggest ZPG instead of ABS (try - for undo)")); 
     if( aix==ISA_AIX_ABX && op<0x100 ) Serial.println(F("INFO: suggest ZPX instead of ABX (try - for undo)")); 
     if( aix==ISA_AIX_ABY && op<0x100 ) Serial.println(F("INFO: suggest ZPY instead of ABY (try - for undo)")); 
-    if( aix==ISA_AIX_REL && PAGE(cmdasm_addr+(int8_t)op)!=PAGE(cmdasm_addr) ) Serial.println(F("INFO: branch to other page (1 cycle extra) (try - for undo)")); 
+    if( aix==ISA_AIX_REL && PAGE(cmdasm_addr+(int8_t)op)!=PAGE(cmdasm_addr) ) Serial.println(F("INFO: branch to other page takes 1 cycle extra (try - for undo)")); 
   }
   // Set the streaming prompt (will only be shown in streaming mode)
   snprintf_P(buf,sizeof buf, PSTR("A:%04x> "),cmdasm_addr); cmd_set_streamprompt(buf);
@@ -94,7 +94,7 @@ static void cmdasm_main( int argc, char * argv[] ) {
   }
   cmdasm_addr= addr;
   cmdasm_addr_first= addr;
-  cmdasm_notify(addr); // Notify first changes address
+  cmdasm_notify(addr); // Notify lowest changed address
   cmdasm_stream(argc,argv); // note that argc/argv have been moved
 }
 
@@ -110,7 +110,7 @@ static const char cmdasm_longhelp[] PROGMEM =
   "- <mnenemonic> is one of the 3 letter opcode abbreviations\n"
   "- <operand> syntax determines addressing mode\n"
   "- in streaming mode '-' undoes previous instruction\n"
-  "- <addr> is 0000..FFFF\n"
+  "- <addr> is 0000..FFFF, but physical memory is limited and mirrored\n"
 ;
   
  
